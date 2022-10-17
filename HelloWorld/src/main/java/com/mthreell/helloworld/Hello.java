42a81a64a4c6de9b852dfdaf7e28b92564936627
Lindsey Logan <lindseylogan12@gmail.com>
2022-10-17 12:41:18 -0400

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/com/mthreell/helloworld/Hello.java
@@ -0,0 +1,17 @@
+
+/**
+ *
+ * @author lindseylogan
+ * email: lindseylogan12@gmail.com
+ * date: 10.17.22
+ * purpose: My first Hello World program in an IDE
+ */
+
+// com.mthreeLL
+package com.mthreell.helloworld;
+
+public class Hello {
+    public static void main(String [] args) {
+        System.out.println("Hello, Lindsey!");
+    }
+}
